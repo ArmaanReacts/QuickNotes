63a36835248cdeea49dab3f63efad64f3f8ae1db
ARMAAN CHAUHAN <armaanchauhan268@gmail.com>
2023-09-04 23:19:03 +0530

    456

--- src/firebase.js
+++ src/firebase.js
@@ -17,6 +17,7 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
+initializeApp(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 export const notesCollection = collection(db, "notes")
